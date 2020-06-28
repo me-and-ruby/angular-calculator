cd46387de89f938f67cc0aa78e588bce69296eae
joash Nyabuti <nyabutijoash@yahoo.com>
2020-06-28 16:58:41 -0500

    angular calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngcalculator';
+  
+  keyPressed="";
+
+  keyPress($event){
+       return this.keyPressed=$event.target.value;
+  }
 }
